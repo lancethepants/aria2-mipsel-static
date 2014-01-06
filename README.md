44fef54eba22c2fcdbbf74dd73072e0274c20208
lancethepants <lancethepants@gmail.com>
2014-01-06 09:02:37 -0700

    Add aria2 script to compile static binary.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 aria2-mipsel-static
 ===================
 
-Script to create static aria2 binary for mipsel devices using entware toolchain
+Script to create static aria2 binary for mipsel devices using entware toolchain.
+
+Make sure the entware toolchain is already in your PATH, and run.
---
+++ aria2.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+set -e
+set -x
+
+mkdir ~/aria2 && cd ~/aria2
+
+BASE=`pwd`
+SRC=$BASE/src
+WGET="wget --prefer-family=IPv4"
+RPATH=/opt/lib
+DEST=$BASE/opt
+LDFLAGS="-L$DEST/lib -Wl,--gc-sections"
+CPPFLAGS="-I$DEST/include -I$DEST/include/ncurses"
+CFLAGS="-mtune=mips32 -mips32 -O3 -ffunction-sections -fdata-sections"	
+CXXFLAGS=$CFLAGS
+CONFIGURE="./configure --prefix=/opt --host=mipsel-linux"
+MAKE="make -j`nproc`"
+mkdir $SRC
+
+######## ####################################################################
+# ZLIB # ####################################################################
+######## ####################################################################
+
+mkdir $SRC/zlib && cd $SRC/zlib
+$WGET http://zlib.net/zlib-1.2.8.tar.gz
+tar zxvf zlib-1.2.8.tar.gz
+cd zlib-1.2.8
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CXXFLAGS=$CXXFLAGS \
+CROSS_PREFIX=mipsel-linux- \
+./configure \
+--prefix=/opt
+
+$MAKE
+make install DESTDIR=$BASE
+
+########### #################################################################
+# OPENSSL # #################################################################
+########### #################################################################
+
+mkdir -p $SRC/openssl && cd $SRC/openssl
+$WGET http://www.openssl.org/source/openssl-1.0.1e.tar.gz
+tar zxvf openssl-1.0.1e.tar.gz
+cd openssl-1.0.1e
+
+cat << "EOF" > openssl.patch
+--- Configure_orig      2013-11-19 11:32:38.755265691 -0700
++++ Configure   2013-11-19 11:31:49.749650839 -0700
+@@ -402,6 +402,7 @@ my %table=(
+ "linux-alpha+bwx-gcc","gcc:-O3 -DL_ENDIAN -DTERMIO::-D_REENTRANT::-ldl:SIXTY_FOUR_BIT_LONG RC4_CHAR RC4_CHUNK DES_RISC1 DES_UNROLL:${alpha_asm}:dlfcn:linux-shared:-fPIC::.so.\$(SHLIB_MAJOR).\$(SHLIB_MINOR)",
+ "linux-alpha-ccc","ccc:-fast -readonly_strings -DL_ENDIAN -DTERMIO::-D_REENTRANT:::SIXTY_FOUR_BIT_LONG RC4_CHUNK DES_INT DES_PTR DES_RISC1 DES_UNROLL:${alpha_asm}",
+ "linux-alpha+bwx-ccc","ccc:-fast -readonly_strings -DL_ENDIAN -DTERMIO::-D_REENTRANT:::SIXTY_FOUR_BIT_LONG RC4_CHAR RC4_CHUNK DES_INT DES_PTR DES_RISC1 DES_UNROLL:${alpha_asm}",
++"linux-mipsel", "gcc:-DL_ENDIAN -DTERMIO -O3 -mtune=mips32 -mips32 -fomit-frame-pointer -Wall::-D_REENTRANT::-ldl:BN_LLONG RC4_CHAR RC4_CHUNK DES_INT DES_UNROLL BF_PTR:${mips32_asm}:o32:dlfcn:linux-shared:-fPIC::.so.\$(SHLIB_MAJOR).\$(SHLIB_MINOR)",
+
+ # Android: linux-* but without -DTERMIO and pointers to headers and libs.
+ "android","gcc:-mandroid -I\$(ANDROID_DEV)/include -B\$(ANDROID_DEV)/lib -O3 -fomit-frame-pointer -Wall::-D_REENTRANT::-ldl:BN_LLONG RC4_CHAR RC4_CHUNK DES_INT DES_UNROLL BF_PTR:${no_asm}:dlfcn:linux-shared:-fPIC::.so.\$(SHLIB_MAJOR).\$(SHLIB_MINOR)",
+EOF
+
+patch < openssl.patch
+
+./Configure linux-mipsel \
+-ffunction-sections -fdata-sections  -Wl,--gc-sections \
+--prefix=/jffs shared no-zlib
+
+make CC=mipsel-linux-gcc AR="mipsel-linux-ar r" RANLIB=mipsel-linux-ranlib
+make install CC=mipsel-linux-gcc AR="mipsel-linux-ar r" RANLIB=mipsel-linux-ranlib INSTALLTOP=$DEST OPENSSLDIR=$DEST/ssl
+
+########## ##################################################################
+# SQLITE # ##################################################################
+########## ##################################################################
+
+mkdir $SRC/sqlite && cd $SRC/sqlite
+$WGET http://www.sqlite.org/2013/sqlite-autoconf-3080200.tar.gz
+tar zxvf sqlite-autoconf-3080200.tar.gz
+cd sqlite-autoconf-3080200
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CXXFLAGS=$CXXFLAGS \
+$CONFIGURE
+
+$MAKE
+make install DESTDIR=$BASE
+
+########### #################################################################
+# LIBXML2 # #################################################################
+########### #################################################################
+
+mkdir $SRC/libxml2 && cd $SRC/libxml2
+$WGET ftp://xmlsoft.org/libxml2/libxml2-2.9.1.tar.gz
+tar zxvf libxml2-2.9.1.tar.gz
+cd libxml2-2.9.1
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CXXFLAGS=$CXXFLAGS \
+$CONFIGURE \
+--without-python
+
+$MAKE
+make install DESTDIR=$BASE
+
+########## ##################################################################
+# C-ARES # ##################################################################
+########## ##################################################################
+
+mkdir $SRC/c-ares && cd $SRC/c-ares
+$WGET http://c-ares.haxx.se/download/c-ares-1.10.0.tar.gz
+tar zxvf c-ares-1.10.0.tar.gz
+cd c-ares-1.10.0
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CXXFLAGS=$CXXFLAGS \
+$CONFIGURE
+
+$MAKE
+make install DESTDIR=$BASE
+
+######### ###################################################################
+# ARIA2 # ###################################################################
+######### ###################################################################
+
+mkdir $SRC/aria2 && cd $SRC/aria2
+$WGET http://downloads.sourceforge.net/project/aria2/stable/aria2-1.18.3/aria2-1.18.3.tar.gz
+tar zxvf aria2-1.18.3.tar.gz
+cd aria2-1.18.3
+
+LDFLAGS="-zmuldefs $LDFLAGS" \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CXXFLAGS=$CXXFLAGS \
+$CONFIGURE \
+--enable-libaria2 \
+--without-libuv \
+--without-appletls \
+--without-gnutls \
+--without-libnettle \
+--without-libgmp \
+--without-libgcrypt \
+--without-libexpat \
+--with-xml-prefix=$DEST \
+ZLIB_CFLAGS="-I$DEST/include" \
+ZLIB_LIBS="-L$DEST/lib" \
+OPENSSL_CFLAGS="-I$DEST/include" \
+OPENSSL_LIBS="-L$DEST/lib" \
+SQLITE3_CFLAGS="-I$DEST/include" \
+SQLITE3_LIBS="-L$DEST/lib" \
+LIBCARES_CFLAGS="-I$DEST/include" \
+LIBCARES_LIBS="-L$DEST/lib" \
+ARIA2_STATIC=yes 
+
+$MAKE \
+LIBS="-static -lz -lssl -lcrypto -lsqlite3 -lxml2 -lcares"
