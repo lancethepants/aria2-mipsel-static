a422a34eab8049721e9acfcc73bd0ecd5c74d602
lancethepants <lancethepants@gmail.com>
2014-06-19 08:40:37 -0600

    Update OpenSSL and sqlite libs.

--- aria2.sh
+++ aria2.sh
@@ -43,9 +43,9 @@ make install DESTDIR=$BASE
 ########### #################################################################
 
 mkdir -p $SRC/openssl && cd $SRC/openssl
-$WGET http://www.openssl.org/source/openssl-1.0.1g.tar.gz
-tar zxvf openssl-1.0.1g.tar.gz
-cd openssl-1.0.1g
+$WGET http://www.openssl.org/source/openssl-1.0.1h.tar.gz
+tar zxvf openssl-1.0.1h.tar.gz
+cd openssl-1.0.1h
 
 cat << "EOF" > openssl.patch
 --- Configure_orig      2013-11-19 11:32:38.755265691 -0700
@@ -76,9 +76,9 @@ make install CC=mipsel-linux-gcc AR="mipsel-linux-ar r" RANLIB=mipsel-linux-ranl
 ########## ##################################################################
 
 mkdir $SRC/sqlite && cd $SRC/sqlite
-$WGET http://sqlite.org/2014/sqlite-autoconf-3080403.tar.gz
-tar zxvf sqlite-autoconf-3080403.tar.gz
-cd sqlite-autoconf-3080403
+$WGET http://sqlite.org/2014/sqlite-autoconf-3080500.tar.gz
+tar zxvf sqlite-autoconf-3080500.tar.gz
+cd sqlite-autoconf-3080500
 
 LDFLAGS=$LDFLAGS \
 CPPFLAGS=$CPPFLAGS \
